1a37c669c1a7bf634df1117d9d818a1b9a54f65b
p.buzzi <p.buzzi@DESKTOP-06PKOIL>
2019-07-28 11:04:13 +0200

    Aggiunto metodi standard

--- LibrettoVoti/src/it/baz/libretto/Voto.java
+++ LibrettoVoti/src/it/baz/libretto/Voto.java
@@ -3,7 +3,35 @@ package it.baz.libretto;
 import java.time.LocalDate;
 
 public class Voto {
+	public Voto(int voto, String corso, LocalDate data) {
+		super();
+		this.voto = voto;
+		this.corso = corso;
+		this.data = data;
+	}
+	
 	private int voto;
 	private String corso;
 	private LocalDate data;
+	
+	public int getVoto() {
+		return voto;
+	}
+	public void setVoto(int voto) {
+		this.voto = voto;
+	}
+	public String getCorso() {
+		return corso;
+	}
+	public void setCorso(String corso) {
+		this.corso = corso;
+	}
+	public LocalDate getData() {
+		return data;
+	}
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+	
+	
 }
